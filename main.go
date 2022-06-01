2c911930ea6cc6c4163761fb5290dcea7da13560
chakrit tnasinpaibool <newchakrit@Administrators-MacBook-Pro-7.local>
2022-06-01 23:33:43 +0700

    fmt

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
 	println("Hello World")
+
+	//prinln ปริ้นค่าง่ายๆ
+	fmt.Println("Hello Chakrit")
+
+	// %d บอกเลขฐาน 10
+	fmt.Printf("Hi %d\n", 10)
+
+	// %v อ่าน value ได้ทุก type
+	fmt.Printf("Hi %v\n", true)
+	fmt.Printf("Hi %v\n", "Golang")
+
+	// %#v อ่าน > value
+	fmt.Printf("Hi %#v\n", "Golang")
+
+	// %T บอก type
+	fmt.Printf("Hi %T\n", "Golang")
+
 }
