62e630da70221cc26a559f976526578fd6a8f03f
chakrit tnasinpaibool <newchakrit@Administrators-MacBook-Pro-7.local>
2022-06-03 16:49:48 +0700

    declaretion type zero-value

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+// var H = 100  -- Global variable --cannot write type inferrence**
+
 func main() {
 	println("Hello World")
 
@@ -15,5 +17,39 @@ func main() {
 	fmt.Printf("Hello %#v \n", "World") //%v print > value  -- more => Hello "World"
 
 	fmt.Printf("%T \n", false) //%T check type
+
+	// ---- Declaretion ----
+	// var -- can change value
+	// const -- cannot change value
+
+	var q int
+	_ = q // declared but never use
+
+	var x int = 10
+	fmt.Println(x)
+
+	//  -- type inferrence --
+	// xx := 9
+	xx := "New"
+	fmt.Println(xx)
+
+	// **** Every declaretion has defuault value (we call zero value!!) ****
+	// Zero value
+	// string : ""
+	// int : 0
+	// float : 0
+	// bool : false
+
+	//exam
+	var xxx int
+	var xxxx string //or
+	a := 0
+	aa := false
+	fmt.Println(xxx)  // 0
+	fmt.Println(xxxx) // "" (empty)
+	fmt.Println(a)    // "" (empty)
+	fmt.Println(aa)   // "" (empty)
+
+	fmt.Println("\n")
 
 }
