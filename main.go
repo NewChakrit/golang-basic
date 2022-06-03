85f6ffc484ebe95f35df2ab4437f2fb316f7194c
chakrit tnasinpaibool <newchakrit@Administrators-MacBook-Pro-7.local>
2022-06-03 16:33:47 +0700

    fmt

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
 	println("Hello World")
+
+	// package "fmt"
+	// fmt.Println("Hello scound world")
+
+	fmt.Printf("Hello %d \n", 10)
+
+	fmt.Printf("Hello %v \n", true)     //%v print value
+	fmt.Printf("Hello %v \n", "World")  //%v print value
+	fmt.Printf("Hello %#v \n", "World") //%v print > value  -- more => Hello "World"
+
+	fmt.Printf("%T \n", false) //%T check type
+
 }
